78831db5718826c36889efbb3e600c813c4d1ef6
Roman Sentsov <zyoma108@gmail.com>
2018-09-02 18:54:44 +0500

    Rename ViewController to MapViewController

---
+++ TestTinkoff/Screen/MapViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MapViewController.swift
+//  TestTinkoff
+//
+//  Created by Roman Sentsov on 31.08.2018.
+//  Copyright © 2018 Roman Sentsov. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    // MARK: - Actions
+    @IBAction func plusClicked(_ sender: Any) {
+        
+    }
+    
+    @IBAction func minusClicked(_ sender: Any) {
+        
+    }
+    
+    @IBAction func currentLocationClicked(_ sender: Any) {
+        
+    }
+    
+}
+
