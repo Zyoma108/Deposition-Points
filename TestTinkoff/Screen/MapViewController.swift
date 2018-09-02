f920ae3f6850f988ec035aa1adf6730888c1c0f5
Roman Sentsov <zyoma108@gmail.com>
2018-09-02 19:41:52 +0500

    Add Location Permissions checking

--- TestTinkoff/Screen/MapViewController.swift
+++ TestTinkoff/Screen/MapViewController.swift
@@ -8,14 +8,44 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
 class MapViewController: UIViewController {
     
-    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet private weak var mapView: MKMapView!
+    private let locationManager = CLLocationManager()
+    private let viewModel = MapViewModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        configure()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        locationManager.requestWhenInUseAuthorization()
+        
+    }
+    
+    private func configure() {
+        mapView.delegate = self
+        locationManager.delegate = self
+    }
+    
+    private func openChangePermissionsAlert() {
+        let title = "Невозможно определить местоположение"
+        let message = "Для отображения текущей позиции приложению необходимо дать доступ к геоданным в настройках"
+        let vc = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "Настройки", style: .default) { _ in
+            guard let url = URL(string: UIApplicationOpenSettingsURLString) else { return }
+            UIApplication.shared.open(url)
+        }
+        let cancelAction = UIAlertAction(title: "Отмена", style: .cancel, handler: nil)
+        vc.addAction(okAction)
+        vc.addAction(cancelAction)
+
+        present(vc, animated: true)
     }
     
     // MARK: - Actions
@@ -28,7 +58,29 @@ class MapViewController: UIViewController {
     }
     
     @IBAction func currentLocationClicked(_ sender: Any) {
-        
+        let currentStatus = CLLocationManager.authorizationStatus()
+        if currentStatus == .denied {
+            openChangePermissionsAlert()
+        }
+    }
+    
+}
+
+extension MapViewController: MKMapViewDelegate {
+    
+}
+
+extension MapViewController: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .authorizedWhenInUse:
+            manager.startUpdatingLocation()
+        case .denied:
+            openChangePermissionsAlert()
+        default:
+            break
+        }
     }
     
 }
---
+++ TestTinkoff/Screen/MapViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  MapViewModel.swift
+//  TestTinkoff
+//
+//  Created by Roman Sentsov on 02.09.2018.
+//  Copyright © 2018 Roman Sentsov. All rights reserved.
+//
+
+import Foundation
+
+class MapViewModel {
+    
+}
